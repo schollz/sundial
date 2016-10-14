7f74c8725d68d8ead283ae9060d869a4abee90e1
Zack <zack.scholl@gmail.com>
2016-10-14 08:49:16 -0400

    Beautified

--- sundial.c
+++ sundial.c
@@ -22,89 +22,89 @@ double const UEPOCH = 946728000.0;
 double const OFFSET = -4*60*60; // EST TIME ZONE
 
 int main(int argc, char **argv) {
-  printf("Currenttime: %2.2f\n",(double)time(NULL));
-  printf("Sunrise: %2.0f\n",sunrise(35.994,-78.8986)); // long, lat for Durham, NC
-  printf("Sunset: %2.0f\n",sunset(35.994,-78.8986));
-	return EXIT_SUCCESS;
+    printf("Currenttime: %2.0f\n",(double)time(NULL));
+    printf("Sunrise: %2.0f\n",sunrise(35.994,-78.8986)); // long, lat for Durham, NC
+    printf("Sunset: %2.0f\n",sunset(35.994,-78.8986));
+    return EXIT_SUCCESS;
 }
 
 double mod360(double x)  {
-	return x - 360.0*floor(x/360.0);
+    return x - 360.0*floor(x/360.0);
 }
 
 double acosD(double x) {
-	if (x >= 1.0 ){
-		return 0.0;
-	}
-	if( x <= -1.0) {
-		return 180.0;
-	}
-	return acos(x) * 180.0 / PI;
+    if (x >= 1.0 ) {
+        return 0.0;
+    }
+    if( x <= -1.0) {
+        return 180.0;
+    }
+    return acos(x) * 180.0 / PI;
 }
 
 double asinD(double x) {
-  return asin(x) * 180.0 / PI;
+    return asin(x) * 180.0 / PI;
 }
 
 double cosD(double x) {
-  return cos(x * PI / 180.0);
+    return cos(x * PI / 180.0);
 }
 
 double sinD(double x) {
-  return sin(x * PI / 180.0);
+    return sin(x * PI / 180.0);
 }
 
 
 double goTime(double x) {
-  return UEPOCH + (x-JEPOCH)*86400.0;
+    return UEPOCH + (x-JEPOCH)*86400.0;
 }
 
 double julianDay(double x) {
-  return (x-UEPOCH)/86400.0 + JEPOCH;
+    return (x-UEPOCH)/86400.0 + JEPOCH;
 }
 
 double jstar(double longitude, double timet) {
-  return 1+floor(julianDay(timet)-0.0009+longitude/360.0+0.5) + 0.0009 - longitude/360;
+    return 1+floor(julianDay(timet)-0.0009+longitude/360.0+0.5) + 0.0009 - longitude/360;
 }
 
 double modD (double a, double b)
 {
-   if(b < 0) //you can check for b == 0 separately and do what you want
-     return modD(a, -b);
-   double ret = (double)((int)a % (int)b);
-   if(ret < 0)
-     ret+=b;
-   return ret;
+    if(b < 0) //you can check for b == 0 separately and do what you want
+        return modD(a, -b);
+    double ret = (double)((int)a % (int)b);
+    if(ret < 0)
+        ret+=b;
+    return ret;
 }
 
 double computeSolarAngle(double latitude, double longitude, int sunrise) {
-  double timet = getTime();
-  timet = timet - modD(timet,86400)-OFFSET;
-  double ma = mod360(357.5291 + 0.98560028*(jstar(longitude,timet)-JEPOCH));
-  double center = 1.9148*sinD(ma) + 0.02*sinD(2.0*ma) + 0.0003*sinD(3.0*ma);
-  double el = mod360(ma + 102.9372 + center + 180.0);
-  double solarNoon = jstar(longitude,timet) + 0.0053*sinD(ma) - 0.0069*sinD(2.0*el);
-  double declination = asinD(sinD(el) * sinD(23.45));
-  double hourAngleInDays = acosD((sinD(-0.83)-sinD(latitude)*sinD(declination))/(cosD(latitude)*cosD(declination))) / 360.0;
-  if (sunrise == 1) {
-    double sunriseTime = goTime(solarNoon - hourAngleInDays);
-    if (sunriseTime < getTime()) {
-      sunriseTime = sunriseTime + 24 * 3600;
+    double timet = getTime();
+    timet = timet - modD(timet,86400)-OFFSET;
+    double ma = mod360(357.5291 + 0.98560028*(jstar(longitude,timet)-JEPOCH));
+    double center = 1.9148*sinD(ma) + 0.02*sinD(2.0*ma) + 0.0003*sinD(3.0*ma);
+    double el = mod360(ma + 102.9372 + center + 180.0);
+    double solarNoon = jstar(longitude,timet) + 0.0053*sinD(ma) - 0.0069*sinD(2.0*el);
+    double declination = asinD(sinD(el) * sinD(23.45));
+    double hourAngleInDays = acosD((sinD(-0.83)-sinD(latitude)*sinD(declination))/(cosD(latitude)*cosD(declination))) / 360.0;
+    if (sunrise == 1) {
+        double sunriseTime = goTime(solarNoon - hourAngleInDays);
+        if (sunriseTime < getTime()) {
+            sunriseTime = sunriseTime + 24 * 3600;
+        }
+        return sunriseTime;
+    } else {
+        return goTime(solarNoon + hourAngleInDays);
     }
-    return sunriseTime;
-  } else {
-    return goTime(solarNoon + hourAngleInDays);
-  }
 }
 
 double sunrise(double latitude, double longitude)  {
-  return computeSolarAngle(latitude,longitude,1);
+    return computeSolarAngle(latitude,longitude,1);
 }
 
 double sunset(double latitude, double longitude)  {
-  return computeSolarAngle(latitude,longitude,0);
+    return computeSolarAngle(latitude,longitude,0);
 }
 
 double getTime() {
-  return (double)time(NULL);
+    return (double)time(NULL);
 }
